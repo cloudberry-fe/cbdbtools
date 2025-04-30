fdd9df765c1d04430cd2de0f5be23dd303464e81
RyanWei <rainmanwy@gmail.com>
2025-04-30 17:01:54 +0800

    debug

--- init_cluster.sh
+++ init_cluster.sh
@@ -16,6 +16,20 @@ if [ "$cluster_type" = "multi" ]; then
   COORDINATOR_HOSTNAME=$(sed -n '/##Coordinator hosts/,/##Segment hosts/p' segmenthosts.conf|sed '1d;$d'|awk '{print $2}')
 fi
 
+LEGACY_VERSION="false"
+
+if [[ "${CLOUDBERRY_RPM}" =~ greenplum ]]; then
+  # Extract the version number, compatible with filenames that do not contain open-source-
+  version=$(echo ${CLOUDBERRY_RPM} | grep -oP 'greenplum-db-\K[0-9.]+')
+  # Get the major version number
+  major_version=$(echo $version | cut -d. -f1)
+  # Determine if the major version number is less than 7
+  if [ $major_version -lt 7 ]; then
+    LEGACY_VERSION="true"
+    echo "Greenplum version is lower than 7"
+  fi
+fi
+
 rm -rf ${INIT_CONFIGFILE} ${MACHINE_LIST_FILE}
 
 echo "ARRAY_NAME=${ARRAY_NAME}" > ${INIT_CONFIGFILE}
@@ -23,14 +37,20 @@ echo "MACHINE_LIST_FILE=${MACHINE_LIST_FILE}" >> ${INIT_CONFIGFILE}
 echo "SEG_PREFIX=${SEG_PREFIX}" >> ${INIT_CONFIGFILE}
 echo "PORT_BASE=${PORT_BASE}" >> ${INIT_CONFIGFILE} 
 echo "declare -a DATA_DIRECTORY=(${DATA_DIRECTORY})" >> ${INIT_CONFIGFILE} 
-echo "COORDINATOR_HOSTNAME=${COORDINATOR_HOSTNAME}" >> ${INIT_CONFIGFILE} 
-echo "COORDINATOR_DIRECTORY=${COORDINATOR_DIRECTORY}" >> ${INIT_CONFIGFILE} 
-echo "COORDINATOR_PORT=${COORDINATOR_PORT}" >> ${INIT_CONFIGFILE} 
 echo "TRUSTED_SHELL=${TRUSTED_SHELL}" >> ${INIT_CONFIGFILE} 
 echo "CHECK_POINT_SEGMENTS=${CHECK_POINT_SEGMENTS}" >> ${INIT_CONFIGFILE}
 echo "ENCODING=${ENCODING}" >> ${INIT_CONFIGFILE}
 echo "DATABASE_NAME=${DATABASE_NAME}" >> ${INIT_CONFIGFILE}
 
+if [ "$LEGACY_VERSION" = "true" ]; then
+  echo "MASTER_DIRECTORY=${COORDINATOR_DIRECTORY}" >> ${INIT_CONFIGFILE} 
+  echo "MASTER_PORT=${COORDINATOR_PORT}" >> ${INIT_CONFIGFILE} 
+  echo "MASTER_DATA_DIRECTORY=${COORDINATOR_DATA_DIRECTORY}" >> ${INIT_CONFIGFILE}
+else
+  echo "COORDINATOR_HOSTNAME=${COORDINATOR_HOSTNAME}" >> ${INIT_CONFIGFILE} 
+  echo "COORDINATOR_DIRECTORY=${COORDINATOR_DIRECTORY}" >> ${INIT_CONFIGFILE} 
+  echo "COORDINATOR_PORT=${COORDINATOR_PORT}" >> ${INIT_CONFIGFILE}  
+fi
 # Add settings for GP6 and earlier versions
 echo "MASTER_HOSTNAME=${COORDINATOR_HOSTNAME}" >> ${INIT_CONFIGFILE} 
 echo "MASTER_DIRECTORY=${COORDINATOR_DIRECTORY}" >> ${INIT_CONFIGFILE} 
@@ -58,7 +78,15 @@ su ${ADMIN_USER} -l -c "export COORDINATOR_DATA_DIRECTORY="${COORDINATOR_DATA_DI
 echo "host all all 0.0.0.0/0 trust" >> ${COORDINATOR_DATA_DIRECTORY}/pg_hba.conf
 
 sed -i '/greenplum_path.sh/d' /home/${ADMIN_USER}/.bashrc
-sed -i '/COORDINATOR_DATA_DIRECTORY/d' /home/${ADMIN_USER}/.bashrc
-echo "export COORDINATOR_DATA_DIRECTORY=${COORDINATOR_DATA_DIRECTORY}" >> /home/${ADMIN_USER}/.bashrc 
 echo "source ${CLOUDBERRY_BINARY_PATH}/greenplum_path.sh" >> /home/${ADMIN_USER}/.bashrc
+
+if [ "$LEGACY_VERSION" = "true" ]; then
+  sed -i '/MASTER_DATA_DIRECTORY/d' /home/${ADMIN_USER}/.bashrc
+  echo "export MASTER_DATA_DIRECTORY=${COORDINATOR_DATA_DIRECTORY}" >> /home/${ADMIN_USER}/.bashrc 
+else
+  sed -i '/COORDINATOR_DATA_DIRECTORY/d' /home/${ADMIN_USER}/.bashrc
+  echo "export COORDINATOR_DATA_DIRECTORY=${COORDINATOR_DATA_DIRECTORY}" >> /home/${ADMIN_USER}/.bashrc 
+fi
+
 su ${ADMIN_USER} -l -c "source ${CLOUDBERRY_BINARY_PATH}/greenplum_path.sh;gpstop -u"
+log_time "Finished init cluster..."
