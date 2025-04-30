2526f4edf0ed72a2c54e911b6000fa2fc2bf06e9
RyanWei <rainmanwy@gmail.com>
2025-04-30 10:03:43 +0800

    debug

--- init_env.sh
+++ init_env.sh
@@ -380,12 +380,13 @@ log_time "Step 6: Setup user no-password access..."
 change_hostname ${COORDINATOR_HOSTNAME}
 rm -rf /home/${ADMIN_USER}/.ssh/
 su ${ADMIN_USER} -l -c "ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ''"
-su ${ADMIN_USER} -l -c "cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys"
-su ${ADMIN_USER} -l -c "ssh-keyscan ${COORDINATOR_HOSTNAME} >> ~/.ssh/known_hosts"
+cat ~/.ssh/id_rsa.pub | sshpass -p "${ADMIN_USER_PASSWORD}" ssh -o StrictHostKeyChecking=no ${ADMIN_USER}@${COORDINATOR_HOSTNAME} "cat >> /home/${ADMIN_USER}/.ssh/authorized_keys"
+#su ${ADMIN_USER} -l -c "cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys"
+#su ${ADMIN_USER} -l -c "ssh-keyscan ${COORDINATOR_HOSTNAME} >> ~/.ssh/known_hosts"
 su ${ADMIN_USER} -l -c "chmod 600 ~/.ssh/authorized_keys"
 su ${ADMIN_USER} -l -c "chmod 644 /home/${ADMIN_USER}/.ssh/known_hosts"
-echo "su ${ADMIN_USER} -l -c \"ssh ${COORDINATOR_HOSTNAME} 'date;exit'"\"
-su ${ADMIN_USER} -l -c "ssh ${COORDINATOR_HOSTNAME} 'date;exit'"
+#echo "su ${ADMIN_USER} -l -c \"ssh ${COORDINATOR_HOSTNAME} 'date;exit'"\"
+#su ${ADMIN_USER} -l -c "ssh ${COORDINATOR_HOSTNAME} 'date;exit'"
 
 
 log_time "Finished env init setting on coordinator..."
