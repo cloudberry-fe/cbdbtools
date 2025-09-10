9ea3ce6ee10867638b7d5619e38616ce92357b53
Ryan Wei <rainmanwy@gmail.com>
2025-09-10 17:41:41 +0800

    add scripts to setup minikube

--- deploycluster.sh
+++ deploycluster.sh
@@ -2,7 +2,7 @@ VARS_FILE="deploycluster_parameter.sh"
 
 source ./${VARS_FILE}
 
-log_time "CBDB tools version is: V1.0_dev20230824"
+log_time "CBDB tools version is: V1.0_dev20250910"
 
 ## Database type and version detection
 # Default values
